59390abb4726779106975f941055323bcebc67d0
Omar Badran <engineer.o.badran@gmail.com>
2022-08-17 16:12:39 +0300

    add @cubes/crypto package

---
+++ packages/crypto/src/index.ts
@@ -0,0 +1,22 @@
+import crypto from 'hypercore-crypto';
+
+export interface KeyPair {
+	publicKey: Buffer;
+	secretKey: Buffer;
+}
+
+export const createKeyPair = (seed?: Buffer): KeyPair => {
+	let kp: KeyPair;
+
+	if (seed) {
+		if (seed.length !== 32) {
+			throw new Error('Invalid seed length');
+		} else {
+			kp = crypto.keyPair(seed);
+		}
+	} else {
+		kp = crypto.keyPair();
+	}
+
+	return kp;
+};
